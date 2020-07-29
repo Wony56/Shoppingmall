5539b92c5b40cd4df40112b0a55b1147292bb200
Wony56 <blackmonkey9256@gmail.com>
2020-07-30 00:12:11 +0900

    엘리님의 코드

--- src/main.js
+++ src/main.js
@@ -1,126 +1,49 @@
-'use strict';
 
-const shirtButton = document.querySelector('.menu__buttons-shirt');
-const pantsButton = document.querySelector('.menu__buttons-pants');
-const skirtButton = document.querySelector('.menu__buttons-skirt');
-const colorButtons = document.querySelectorAll('.button-color');
-const list = document.querySelector('.list__clothes');
+// Fetch the items from the JSON file.
+function loadItems() {
+    return fetch('data/data.json')
+        .then(res => res.json())
+        .then(json => json.items);
+}
 
+// Update the list with the given items.
+function displayItems(items) {
+    const container = document.querySelector('.items');
+    container.innerHTML = items.map(item => createHTMLString(item)).join('');
+}
 
+// Create HTML list from the given data item.
+function createHTMLString(item) {
+    return `
+        <li class="item">
+            <img src="${item.image}" alt="${item.type}" class="item__thumbnail">
+            <span class="item__description">${item.gender}, ${item.size}</span>
+        </li>
+    `
+}
 
-const init = () => {
-    generateDataset(100);
-    clothesList.forEach(clothe => {
-        const item = document.createElement('li');
-        item.className = 'list__clothes-item';
-        item.dataset.type = clothe.type;
-        item.dataset.color = clothe.color;
-        const img = document.createElement('img');
-        img.className = 'item__image';
-        img.src = clothe.url;
-        const gender = document.createElement('span');
-        gender.className = 'item__gender';
-        gender.textContent = clothe.gender + ',';
-        const size = document.createElement('span');
-        size.className = 'item__size';
-        size.textContent = clothe.size;
-        item.appendChild(img);
-        item.appendChild(gender);
-        item.appendChild(size);
-        list.appendChild(item);
-    });
+function onButtonClick(event, items) {
+    const { key, value } = event.target.dataset;
 
-    colorButtons.forEach(colorButton => {
-        colorButton.addEventListener('click', colorChange);
-    });
-
-    shirtButton.addEventListener('click', listByType);
-    pantsButton.addEventListener('click', listByType);
-    skirtButton.addEventListener('click', listByType);
-};
-
-const removeAllItems = () => {
-    document.querySelectorAll('.list__clothes-item').forEach(item => {
-        list.removeChild(item);
-    });
-};
-
-const listByColor = (color) => {
-    removeAllItems();
-    clothesList.filter(clothe => clothe.color === color).forEach(clothe => {
-        const item = document.createElement('li');
-        item.className = 'list__clothes-item';
-        item.dataset.type = clothe.type;
-        item.dataset.color = clothe.color;
-        const img = document.createElement('img');
-        img.className = 'item__image';
-        img.src = clothe.url;
-        const gender = document.createElement('span');
-        gender.className = 'item__gender';
-        gender.textContent = clothe.gender + ',';
-        const size = document.createElement('span');
-        size.className = 'item__size';
-        size.textContent = clothe.size;
-        item.appendChild(img);
-        item.appendChild(gender);
-        item.appendChild(size);
-        list.appendChild(item);
-    });
-};
-
-const listByType = (e) => {
-    const type = e.target.dataset.type;
-    removeAllItems();
-    clothesList.filter(clothe => clothe.type === type).forEach(clothe => {
-        const item = document.createElement('li');
-        item.className = 'list__clothes-item';
-        item.dataset.type = clothe.type;
-        item.dataset.color = clothe.color;
-        const img = document.createElement('img');
-        img.className = 'item__image';
-        img.src = clothe.url;
-        const gender = document.createElement('span');
-        gender.className = 'item__gender';
-        gender.textContent = clothe.gender + ',';
-        const size = document.createElement('span');
-        size.className = 'item__size';
-        size.textContent = clothe.size;
-        item.appendChild(img);
-        item.appendChild(gender);
-        item.appendChild(size);
-        list.appendChild(item);
-    });
-};
-
-const colorChange = (e) => {
-    if(e.target.dataset.color === 'blue'){
-        shirtButton.style.backgroundImage = "url('./img/blue_t.png')";
-        pantsButton.style.backgroundImage = "url('./img/blue_p.png')";
-        skirtButton.style.backgroundImage = "url('./img/blue_s.png')";
-        shirtButton.dataset.type = 'blue_t';
-        pantsButton.dataset.type = "blue_p";
-        skirtButton.dataset.type = "blue_s";
-        listByColor('blue');
-    }else if(e.target.dataset.color === 'yellow') {
-        shirtButton.style.backgroundImage = "url('./img/yellow_t.png')";
-        pantsButton.style.backgroundImage = "url('./img/yellow_p.png')";
-        skirtButton.style.backgroundImage = "url('./img/yellow_s.png')";
-        shirtButton.dataset.type = 'yellow_t';
-        pantsButton.dataset.type = "yellow_p";
-        skirtButton.dataset.type = "yellow_s";
-        listByColor('yellow')
-    }else if(e.target.dataset.color === 'pink') {
-        shirtButton.style.backgroundImage = "url('./img/pink_t.png')";
-        pantsButton.style.backgroundImage = "url('./img/pink_p.png')";
-        skirtButton.style.backgroundImage = "url('./img/pink_s.png')";
-        shirtButton.dataset.type = 'pink_t';
-        pantsButton.dataset.type = "pink_p";
-        skirtButton.dataset.type = "pink_s";
-        listByColor('pink');
+    if(key == null || value == null) {
+        return;
     }
-};
 
+    const filtered = items.filter(item => item[key] === value);
+    displayItems(filtered);
+}
 
+function setEventListener(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons') // 이벤트 위임
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
 
-
-init();
+// main
+loadItems()
+    .then(items=> {
+        displayItems(items);
+        setEventListener(items);
+    })
+    .catch(err => console.error(err));
